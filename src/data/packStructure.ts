5f373959c7dbdbddac285db60a5bc6fded7bb49c
Bulaxy <rogerhiufungcheng@gmail.com>
2026-05-10 21:17:57 +1000

    chore: Add missing

--- src/data/packStructure.ts
+++ src/data/packStructure.ts
@@ -1,9 +1,16 @@
 /**
  * Maps campaign/group names to their constituent ArkhamDB pack codes.
  * Used to group individual card packs into logical collections for filtering.
+ * Orders here matters
  */
 export const PACK_STRUCTURE: Record<string, string[]> = {
+  parallel: ["aof", "aon", "aon_encounter", "bad", "bad_encounter", "btb", "btb_encounter", "enc", "enc_encounter", "hfa", "ltr", "ltr_encounter", "otr", "pap", "ptr", "rod", "rod_encounter", "rop", "rop_encounter", "rtr", "rtr_encounter"],
+  return: ["rtdwl", "rtdwl_encounter", "rtnotz", "rtnotz_encounter", "rtptc", "rtptc_encounter", "rttcu", "rttcu_encounter", "rttfa", "rttfa_encounter"],
+  investigator: ["and", "car", "har", "jac", "mar", "mig", "nat", "ste", "tom", "win"],
+  side: ["blbe", "blbe_encounter", "blob", "blob_encounter", "coh", "coh_encounter", "cotr", "cotr_encounter", "film_fatale", "film_fatale_encounter", "fof", "fof_encounter", "guardians", "guardians_encounter", "hotel", "hotel_encounter", "lol", "lol_encounter", "mtt", "mtt_encounter", "tmg", "tmg_encounter", "wog", "wog_encounter"],
+  promo: ["bob", "dre", "hoth", "iotv", "promo", "tdg", "tdor", "tftbw"],
   core: ["core", "core_encounter"],
+  rcore: ["rcore"],
   dwl: ["bota", "bota_encounter", "dwl", "dwl_encounter", "litas", "litas_encounter", "tece", "tece_encounter", "tmm", "tmm_encounter", "uau", "uau_encounter", "wda", "wda_encounter"],
   ptc: ["apot", "apot_encounter", "bsr", "bsr_encounter", "dca", "dca_encounter", "eotp", "eotp_encounter", "ptc", "ptc_encounter", "tpm", "tpm_encounter", "tuo", "tuo_encounter"],
   tfa: ["hote", "hote_encounter", "sha", "sha_encounter", "tbb", "tbb_encounter", "tcoa", "tcoa_encounter", "tdoy", "tdoy_encounter", "tfa", "tfa_encounter", "tof", "tof_encounter"],
@@ -14,11 +21,7 @@ export const PACK_STRUCTURE: Record<string, string[]> = {
   fhv: ["fhvc", "fhvp"],
   tsk: ["tskc", "tskp"],
   tdc: ["tdcc", "tdcp"],
-  parallel: ["aof", "aon", "aon_encounter", "bad", "bad_encounter", "btb", "btb_encounter", "enc", "enc_encounter", "hfa", "ltr", "ltr_encounter", "otr", "pap", "ptr", "rod", "rod_encounter", "rop", "rop_encounter", "rtr", "rtr_encounter"],
-  return: ["rtdwl", "rtdwl_encounter", "rtnotz", "rtnotz_encounter", "rtptc", "rtptc_encounter", "rttcu", "rttcu_encounter", "rttfa", "rttfa_encounter"],
-  investigator: ["and", "car", "har", "jac", "mar", "mig", "nat", "ste", "tom", "win"],
-  side: ["blbe", "blbe_encounter", "blob", "blob_encounter", "coh", "coh_encounter", "cotr", "cotr_encounter", "film_fatale", "film_fatale_encounter", "fof", "fof_encounter", "guardians", "guardians_encounter", "hotel", "hotel_encounter", "lol", "lol_encounter", "mtt", "mtt_encounter", "tmg", "tmg_encounter", "wog", "wog_encounter"],
-  promo: ["bob", "dre", "hoth", "iotv", "promo", "tdg", "tdor", "tftbw"],
+  "core 2026": ["core_2026", "core_2026_encounter"],
 };
 
 /**
